3714abf373538fd0078946838ac664ecc95dc55c
Cleverton Inacio <cleverton.inacio.110@ufrn.edu.br>
2022-05-11 00:07:47 -0300

    Atualiza cabeçalho

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,25 @@
-import { FiAlignLeft } from 'react-icons/fi'
-import { ReactComponent as PizzaLogo } from '../../assets/pizza-logo.svg'
+import { FaCaretDown } from 'react-icons/fa';
+import { FiAlignLeft } from 'react-icons/fi';
+import { ReactComponent as PizzaLogo } from '../../assets/pizza-logo.svg';
+
+
+import "./header.scss";
+
+
 export default function Header() {
   return (
     <header>
       <div className="header-container">
-        <FiAlignLeft size={33} />
-        <div className="logo-div">
-          <PizzaLogo />
-          <h3>CantinhodaMari</h3>
+        <div className="header-logo">
+          <FiAlignLeft size={28} />
+          <div className="logo-div">
+            <PizzaLogo className='logo-svg' />
+            <h3>CANTINHO DA MARI</h3>
+          </div>
+        </div>
+        <div className='delivery-button'>
+          <h4>Delivery</h4>
+          <FaCaretDown />
         </div>
       </div>
     </header>
